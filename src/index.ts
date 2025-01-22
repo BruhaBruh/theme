5a55a9b41b432df7c55db759d5104143ed6c1833
BruhaBruh <drugsho.jaker@gmail.com>
2025-01-23 02:32:51 +0500

    chore: tailwindcss plugin config now is partial

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import plugin from 'tailwindcss/plugin';
 import { configExecutor } from './config/config-executor';
 import { Config } from './config/schema/config';
 
-export const themePlugin = plugin.withOptions<Config>(
+export const themePlugin = plugin.withOptions<Partial<Omit<Config, 'output'>>>(
   (config) => {
     const executor = configExecutor(config);
     return (api) => executor.applyTailwind(api);
